6f6d590206d454b34df693ec1339120ce2e02bb1
slenderballa <gswe4645@740s-pl-132.local>
2015-10-23 09:25:21 -0600

    Added popup
    It runs

--- src/chat/controller/ChatController.java
+++ src/chat/controller/ChatController.java
@@ -1,5 +1,25 @@
 package chat.controller;
 
-public class ChatController {
+import chat.model.Chatbot;
+import chat.view.ChatView;
+
+public class ChatController 
+{
+	
+	private Chatbot simpleBot;
+	private ChatView display;
+	
+	public ChatController()
+	{
+		display = new ChatView();
+		String userName = display.grabAnswer("What is your name?");
+		simpleBot = new Chatbot(userName);
+	}
+	
+	
+	public void start()
+	{
+		display.showResponse("Hello " + simpleBot.getUserName());
+	}
 
 }
